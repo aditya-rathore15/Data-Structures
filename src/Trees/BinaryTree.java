44fdc72dd3e3e572c02482e22415dab84aea9ff5
aditya-rathore15 <rathoreaditya.work@gmail.com>
2025-09-10 14:09:21 -0400

    Implement Binary Tree

---
+++ src/Trees/BinaryTree.java
@@ -0,0 +1,43 @@
+package Trees;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {}
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left,  TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class BinaryTree {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+//        Approach 2
+//        TreeNode leftChild = new TreeNode(2);
+//        TreeNode rightChild = new TreeNode(3);
+//        TreeNode root = new TreeNode(1, leftChild, rightChild);
+
+
+//        Approach 3
+//        TreeNode root = new TreeNode(1,
+//                new TreeNode(2, new TreeNode(4), new TreeNode(5)),
+//                new TreeNode(3));
+
+
+        System.out.println("Done!");
+    }
+}
